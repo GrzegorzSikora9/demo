2a94dba830cbf33126476d3559756b85bba86732
FP\gsikora <gsikora@future-processing.com>
2021-07-15 18:57:02 +0200

    Delete and put method working, tested via postman

--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -33,12 +33,27 @@ public class FakePersonDataAccessService implements PersonDao {
 
     @Override
     public int deletePersonById(UUID id) {
-        return 0;
+        // in Java 8 there is no method like .isEmpty. Insted of this I used .!isPresent
+        Optional<Person> personMaybe = selectPersonById(id); // commented line above
+        if (!personMaybe.isPresent()){
+            return 0;
+        }
+        DB.remove(personMaybe.get());
+        return 1;
     }
 
     @Override
-    public int updatePersonById(UUID id, Person person) {
-        return 0;
+    public int updatePersonById(UUID id, Person update) {
+        return selectPersonById(id)
+                .map(person -> {
+                    int indexOfPersonToUpdate = DB.indexOf(person);
+                    if (indexOfPersonToUpdate >= 0) {
+                        DB.set(indexOfPersonToUpdate, new Person(id, update.getName()));
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
     }
 
 }
