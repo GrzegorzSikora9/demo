53f364a3db5c00121eed125e349e5d4225bb23ec
FP\gsikora <gsikora@future-processing.com>
2021-07-16 16:27:00 +0200

    Connection to postgres

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -1,12 +1,15 @@
 package com.example.demo.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.internal.NotNull;
+
 
 import java.util.UUID;
 
 public class Person {
 
     private final UUID id;
+    @NotNull // I cannot use adnotation is @NotBlank, instead of this I used @NotNull -> I cannot post a null object, however i can post blank 'name'.
     private final String name;
 
     public Person(@JsonProperty("id") UUID id,@JsonProperty("name") String name) {
